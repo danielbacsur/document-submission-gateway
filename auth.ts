0dd3530f0a9e0b15f12a5a6fb8dc774dcd75067e
Daniel Bacsur <contact@danielbacsur.com>
2025-01-27 20:34:51 +0100

    feat: add drizzle adapter to the auth configuration

--- auth.ts
+++ auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import Resend from "next-auth/providers/resend";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { postgres } from "@/postgres";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(postgres),
   providers: [
     Resend({
       from: "daniel@mail.danielbacsur.dev",
